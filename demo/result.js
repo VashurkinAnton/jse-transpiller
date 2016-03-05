968f18ab9fc84a563ed60e58410555ce2be2f22f
VashurkinAnton <cronosfera2@yandex.ru>
2016-03-05 14:40:42 +0300

    Name fixing.

--- demo/result.js
+++ demo/result.js
@@ -1,16 +1,18 @@
-function(){
-	function component(){
-		return [
-			(function(Elm){var elm = Elm();elm.open("h1");elm.text("Header 1");return elm.close(); }).call(this, JSE),
-			(function(Elm){var elm = Elm();elm.component("h2",h2);elm.text("Header 2");return elm.close(); }).call(this, JSE),
-			(function(Elm){var elm = Elm();elm.component("h3",h3);elm.text("Header 3");return elm.close(); }).call(this, JSE),
-			(function(Elm){var elm = Elm();elm.component("h4",h4);elm.text("Header 4");return elm.close(); }).call(this, JSE),
-			(function(Elm){var elm = Elm();elm.component("h5",h5);elm.text("Header 5");return elm.close(); }).call(this, JSE),
-			(function(Elm){var elm = Elm();elm.component("h6",h6);elm.text("Header 6");return elm.close(); }).call(this, JSE)
-		];
-	}
+var JSE = require('../src/jse-pipe-elm.js');
 
-	return (function(Elm){var elm = Elm();elm.open("div",{"class":"data"});
-		elm.component.bind({selfClosing:true})("component",component);
-	return elm.close(); }).call(this, JSE);
+function component(){
+	return [
+		(function(Elm){var elm = Elm();elm.open("h1");elm.text("Header 1");return elm.close(); }).call(this, JSE),
+		(function(Elm){var elm = Elm();elm.open("h2");elm.text("Header 2");return elm.close(); }).call(this, JSE),
+		(function(Elm){var elm = Elm();elm.open("h3");elm.text("Header 3");return elm.close(); }).call(this, JSE),
+		(function(Elm){var elm = Elm();elm.open("h4");elm.text("Header 4");return elm.close(); }).call(this, JSE),
+		(function(Elm){var elm = Elm();elm.open("h5");elm.text("Header 5");return elm.close(); }).call(this, JSE),
+		(function(Elm){var elm = Elm();elm.open("h6");elm.text("Header 6");return elm.close(); }).call(this, JSE)
+	];
 }
+
+var a = (function(Elm){var elm = Elm();elm.open("div",{"class":"data"});
+	elm.component("component",component);elm.close();
+return elm.close(); }).call(this, JSE);
+
+console.log(component());
--- demo/test.jsx
+++ demo/test.jsx
@@ -1,16 +1,18 @@
-function(){
-	function component(){
-		return [
-			<h1><text>Header 1</text></h1>,
-			<h2><text>Header 2</text></h2>,
-			<h3><text>Header 3</text></h3>,
-			<h4><text>Header 4</text></h4>,
-			<h5><text>Header 5</text></h5>,
-			<h6><text>Header 6</text></h6>
-		];
-	}
+var JSE = require('../src/jse-pipe-elm.js');
 
-	return <div class='data'>
-		<component />
-	</div>;
+function component(items){
+	return [
+		амввввввввв,
+		<h2><text>Header 2</text></h2>,
+		<h3><text>Header 3</text></h3>,
+		<h4><text>Header 4</text></h4>,
+		<h5><text>Header 5</text></h5>,
+		<h6><text>Header 6</text></h6>
+	];
 }
+
+var a = <div class='data'>
+	<component></component>
+</div>;
+
+console.log(a);
